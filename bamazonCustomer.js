9628b905637e4f831000c4e1698d5ebc21b63bd8
Lindsey Paluso <lindzymarie@gmail.com>
2020-05-11 17:55:25 -0600

    adding inital files

---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "8889",
+    user: "root",
+    password: "root",
+    database: "bamazon"
+
+});
+
